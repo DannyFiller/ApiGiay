d43114b6fa241ff7b6d394d571638cca3bb5c4d9
DannyFillerSD <tuanvakhoi2991@gmail.com>
2024-07-05 17:56:54 +0700

    del shoe

--- router/shoeRouter.js
+++ router/shoeRouter.js
@@ -4,5 +4,6 @@ const router = require("express").Router();
 router.get("/",ShoeController.getAllShoe);
 router.post("/",ShoeController.postShoe);
 router.put("/:id",ShoeController.editShoe);
+router.delete("/:id",ShoeController.deleteShoe);
 
 module.exports = router;
